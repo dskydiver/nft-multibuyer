65eeae29b376b3979a6833f066e74a2a62e969a4
dskydiver <jon@ebk.tech>
2024-11-06 06:15:53 -0600

    update deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.27",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ]
+  },
+  networks: {
+    ropsten: {
+      url: "https://apechain-mainnet.g.alchemy.com/v2/gw7asj34P-gXulGNGFyUm_bbqF5ROV2O",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+}
 
-export default config;
+export default config
